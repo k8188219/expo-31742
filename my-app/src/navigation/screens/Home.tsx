468161c2ca95b08efee639dcb7a33e8fd8b3ee53
k8188219 <k8188219@gmail.com>
2024-12-13 01:48:22 +0000

    feat: first commit

---
+++ my-app/src/navigation/screens/Home.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View, Button, Text } from 'react-native';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+});
