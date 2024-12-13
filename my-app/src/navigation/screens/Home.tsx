e209b7ff82a92408d50c70963349638f475dbfb4
k8188219 <k8188219@gmail.com>
2024-12-13 01:58:07 +0000

    test code

--- my-app/src/navigation/screens/Home.tsx
+++ my-app/src/navigation/screens/Home.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useRef, useState } from 'react';
 import { StyleSheet, View, Button, Text } from 'react-native';
 
+
+let mountedCount = 0;
+
 export function Home() {
+
+  // use Ref to prevent setCount updates and renders different mountedCount
+  const ref = useRef(mountedCount);
+
+  useEffect(() => {
+    console.error('mounted');
+    mountedCount++;
+
+    return () => {
+      console.error('un-mounted');
+    }
+  }, []);
+
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
+      <Text style={{ fontSize: 36 }}>index.tsx</Text>
+      <Text style={{ fontSize: 36, color: 'red' }}>Mounted {ref.current} times</Text>
+      <Button title={`Presssed ${count} times`} onPress={() => { setCount(v => v + 1) }} />
     </View>
   );
 }
